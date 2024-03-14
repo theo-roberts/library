bab88c82d2e99d1bab6fba66cf55e0d5417f55a1
Theo Roberts <theo10roberts@gmail.com>
2024-03-14 18:42:54 +0000

    add loop

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ function Book(title, author, isRead) {
     this.isRead = isRead;
 };
 
-//work out how to get the new book to display in the hmtl
+
+
+//overwriting each book, and not showing in console log. work out how to
+//get it to loop thru.
 
 document.querySelector("#submitBtn").addEventListener('click', function(){
 
     title = document.getElementById("bookTitle").value;
     author = document.getElementById("bookAuthor").value;
     isRead = document.getElementById("isRead").value;
-
     newBook = new Book(title, author, isRead)
+
     myLibrary.push(newBook)
     event.preventDefault()
+    
     document.getElementById('bookForm').style.display = "none";
-    console.log(myLibrary)
-    let shelf = document.getElementById("shelf");
+
+    function displayInShelf() {
+        document.getElementById("shelf").innerHTML = 
+        '<div>' + title + ", " + author + '</div>'
+    };
+
+    myLibrary.forEach(displayInShelf(Book));
+    console.log(Book)
+    
 });
+
+
+
 
 document.getElementById('newBookBtn').addEventListener('click', function(){
     document.getElementById('bookForm').style.display = "block";
