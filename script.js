2b887b7308f7391589e24c575cf827ee6f960a8b
Theo Roberts <theo10roberts@gmail.com>
2024-03-19 14:50:18 +0000

    add checkbox if read

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 const myLibrary = [];
 
-function Book(title, author, isRead, bookID) {
+function Book(title, author, bookID) {
     this.title = title;
     this.author = author;
-    this.isRead = isRead;
     this.bookID = bookID;
 };
 
 let counter = 0
 
-//work out how to add the checkbox to the display.
+
 
 document.querySelector("#submitBtn").addEventListener('click', function(){
 
-        document.getElementById('bookForm').style.display = "none";
+    document.getElementById('bookForm').style.display = "none";
 
-        title = document.getElementById("bookTitle").value;
-        author = document.getElementById("bookAuthor").value;
-        isRead = document.getElementById("isRead").value;
-        bookID = counter;
-        newBook = new Book(title, author, isRead, bookID)
+    title = document.getElementById("bookTitle").value;
+    author = document.getElementById("bookAuthor").value;
+    bookID = counter;
+    newBook = new Book(title, author, bookID)
 
-        myLibrary.push(newBook)
+    myLibrary.push(newBook)
 
-        function displayInShelf() {
-            let bookContainer = document.createElement('div');
-            let bookChild = document.createElement('div');
-            let text = document.createTextNode(myLibrary[i].title + ", " + myLibrary[i].author)
-            bookChild.appendChild(text);
-            bookContainer.appendChild(bookChild)
-            document.getElementById("shelf").appendChild(bookContainer)
-        };
+    function displayInShelf() {
+        let bookContainer = document.createElement('div');
+                
+        let bookTitle = document.createElement('div');
+        bookTitle.setAttribute("class", "bookTitle"); 
+        let bookTitleText = document.createTextNode(myLibrary[i].title)
+        bookTitle.appendChild(bookTitleText);
+    
+        let bookAuthor = document.createElement('div');
+        bookAuthor.setAttribute("class", "bookAuthor"); 
+        let bookAuthorText = document.createTextNode(myLibrary[i].author)
+        bookAuthor.appendChild(bookAuthorText);
+    
+        let readCheckbox = document.createElement('input')
+        readCheckbox.setAttribute("type", "checkBox", "name", "readStatus")
+    
+        let checkBoxLabel = document.createElement('label')
+        checkBoxLabel.setAttribute("for", "readStatus")
+        let checkBoxLabelText = document.createTextNode("Read")
+        checkBoxLabel.appendChild(checkBoxLabelText)
+    
+        bookContainer.appendChild(bookTitle)
+        bookContainer.appendChild(bookAuthor)
+        bookContainer.appendChild(checkBoxLabel)
+        bookContainer.appendChild(readCheckbox)
+    
+        document.getElementById("shelf").appendChild(bookContainer)
+    };
 
-        for (i=counter; i<myLibrary.length; i++){
+    for (i=counter; i<myLibrary.length; i++){
         displayInShelf(Book)
-        };
+    };
 
-        counter++
-        event.preventDefault()
-    console.log(isRead)
+    counter++
+
+    event.preventDefault()
+
+    console.log(myLibrary)
 });
-
-
 
 
 document.getElementById('newBookBtn').addEventListener('click', function(){
